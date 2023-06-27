4a072d288661dd5d2990d2b3dfa58cc14159d2ca
Priti Paul <75872164+pritipaul@users.noreply.github.com>
2023-06-27 21:28:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 # import tensorflow as tf
 from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.utils import to_categorical
