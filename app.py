a17cf32180c05c56f715bfa5a6ccd0781a57969a
Priti Paul <75872164+pritipaul@users.noreply.github.com>
2024-02-28 10:47:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,10 +18,7 @@ y = data['Dementia']
 x = data.drop('Dementia', axis=1)
 
 # Split into training and testing data
-# X_train, X_test, y_train, y_test = train_test_split ( X, y,
-                                                     test_size = 0.2,
-                                                     random_state = 1,
-                                                     stratify = y)
+# X_train, X_test, y_train, y_test = train_test_split ( X, y,test_size = 0.2,random_state = 1,stratify = y)
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=1,stratify = y)
 
 # Standardize the data
